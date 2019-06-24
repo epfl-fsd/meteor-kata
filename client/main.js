05b0b15cc9c225716a6c00946e8dfcb7cfff40f2
Dominique Quatravaux <dominique@quatravaux.org>
2019-06-24 09:26:50 +0200

    Kit de démarrage pour Vue.js

--- client/main.js
+++ client/main.js
@@ -1,22 +1,9 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import Vue from "vue"
+import App from '/imports/ui/App.vue'
+import "./main.html"
 
-import './main.html';
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Meteor.startup(() => {
+  new Vue({
+    render: h => h(App),
+  }).$mount('#pourvue');
 });
---
+++ imports/ui/App.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="app">
+  <h1>Exemple avec Vue 2.x</h1>
+  <p>
+    Vous avez pressé le bouton {{count}} fois.
+  </p>
+  <button @click="addOne">{{ buttonLabel }}</button>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+      buttonLabel: 'Cliquez-moi !',
+    }
+  },
+  methods: {
+    addOne() { this.count++ }
+  }
+};
+</script>
+
+<style>
+body {
+  margin: 30px;
+}
+
+a {
+  color: #40b883;
+  text-decoration: none;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+</style>
