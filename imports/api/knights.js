e725b24d91ef1972fc3dcd159e9e5d61544f0edc
Dominique Quatravaux <dominique@quatravaux.org>
2019-06-24 11:04:59 +0200

    Définition de la collection "knights"
    Seulement sur le serveur pour l'instant (même si, conformément à
    https://katacoda.com/domq/meteor-intro et grâce au module autopublish
    de Meteor, les données commencent aussitôt à être synchronisées vers
    les clients)

---
+++ imports/api/knights.js
@@ -0,0 +1,1 @@
+export const Knights = new Mongo.Collection('knights');
--- server/main.js
+++ server/main.js
@@ -1,4 +1,5 @@
 import { Meteor } from 'meteor/meteor';
+import { Knights } from '/imports/api/knights';
 
 Meteor.startup(() => {
   // code to run on server at startup
